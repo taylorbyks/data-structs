cde1e451a9c6c437a509782334e0eca67f502437
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-06-15 20:16:02 -0300

    Add main at queue

---
+++ queue/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Estrutura de Dados - 2021
+//
+//  Created by Taylor Schinvelski on 13/03/21.
+//
+
+#include "functions.c"
+
+int main(void){
+  queue.size = 0;
+  menu();
+  return 0;
+}
