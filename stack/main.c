b7d1428f79290182abe5259bcd72e3d28b0ca5c4
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-06-15 20:16:12 -0300

    Add main at stack

---
+++ stack/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Estrutura de Dados - 2021
+//
+//  Created by Taylor Schinvelski on 04/03/21.
+//
+
+#include "functions.c"
+
+int main(void){
+  stack.top = -1;
+  menu();
+  return 0;
+}
