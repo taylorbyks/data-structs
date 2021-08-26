f885d7b5162fca190d5591db193c256485d6fee1
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-08-26 16:26:48 -0300

    Add menu to select algorithms

--- comparison sorting/main.cpp
+++ comparison sorting/main.cpp
@@ -45,17 +45,112 @@ void insertionSort(int array[], int size) {
   cout << "Finalizado - Duração: " << (clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << endl << endl;
 }
 
+void selectionSort(int array[], int size) {
+  clock_t start = clock();
+  
+  // code
+  
+  cout << "Finalizado - Duração: " << (clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << endl << endl;
+}
 
-int main() {
+void bubbleSort(int array[], int size) {
+  clock_t start = clock();
+  
+  // code
+  
+  cout << "Finalizado - Duração: " << (clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << endl << endl;
+}
+
+void quickSort(int array[], int size) {
+  clock_t start = clock();
+  
+  // code
+  
+  cout << "Finalizado - Duração: " << (clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << endl << endl;
+}
+
+void mergeSort(int array[], int size) {
+  clock_t start = clock();
+  
+  // code
+  
+  cout << "Finalizado - Duração: " << (clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << endl << endl;
+}
+
+void shellSort(int array[], int size) {
+  clock_t start = clock();
+  
+  // code
+  
+  cout << "Finalizado - Duração: " << (clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << endl << endl;
+}
+
+void menu() {
+  int option = 0;
   int array[] = {1, 6, 5, 3, 2, 8, 12, 2, 1, 8, 9, 11, 4, 15, 19, 20, 40, 45, 6, 7, 13, 16};
   int arraySize = sizeof(array)/sizeof(array[0]);
   
   printArray(array, arraySize);
   
-  cout << "Usando isertion sort: " << endl;
-  insertionSort(array, arraySize);
+  cout << "Escolha um algoritmo para ordenar o vetor:" << endl;
+  cout << "1 - Insertion Sort" << endl;
+  cout << "2 - Selection Sort" << endl;
+  cout << "3 - Bubble Sort" << endl;
+  cout << "4 - Quick Sort" << endl;
+  cout << "5 - Merge Sort" << endl;
+  cout << "6 - Shell Sort" << endl;
   
-  printArray(array, arraySize);
+  cin >> option;
+  
+  switch (option) {
+    case 1:
+      cout << "Usando Insertion Sort: " << endl;
+      insertionSort(array, arraySize);
+      printArray(array, arraySize);
+      break;
+      
+    case 2:
+      cout << "Usando Selection Sort: " << endl;
+      selectionSort(array, arraySize);
+      printArray(array, arraySize);
+      break;
+      
+    case 3:
+      cout << "Usando Bubble Sort: " << endl;
+      bubbleSort(array, arraySize);
+      printArray(array, arraySize);
+      break;
+      
+    case 4:
+      cout << "Usando Quick Sort: " << endl;
+      quickSort(array, arraySize);
+      printArray(array, arraySize);
+      break;
+    
+    case 5:
+      cout << "Usando Merge Sort: " << endl;
+      mergeSort(array, arraySize);
+      printArray(array, arraySize);
+      break;
+      
+    case 6:
+      cout << "Usando Shell Sort: " << endl;
+      shellSort(array, arraySize);
+      printArray(array, arraySize);
+      break;
+      
+    default:
+      cout << "Selecione uma opção válida " << endl;
+      break;
+  }
+  
+  system("read -r -p \"Pressione uma tecla para continuar...\"");
+  system("clear");
+  menu();
+}
+
+int main() {
+  menu();
   
   return 0;
 }
